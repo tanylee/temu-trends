a8d3af32ed50e27c9ceb02aaf601569f7fc55120
tanylee <1successonlinebusiness@gmail.com>
2025-10-02 04:45:48 -0700

    Delete scripts directory

--- scripts/utils/dedupe.js
+++
@@ -1,8 +0,0 @@
-export const dedupeBy = (arr, keyFn) => {
-  const m = new Map();
-  for (const x of arr) {
-    const k = keyFn(x);
-    if (!m.has(k)) m.set(k, x);
-  }
-  return [...m.values()];
-};
